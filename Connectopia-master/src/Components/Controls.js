a259a03b13b685e88e6b4089be12fedb9fa8d5e4
praneethak <pkanvapuri@gmail.com>
2023-07-27 12:55:48 +0530

    Mic button fixed

--- Connectopia-master/src/Components/Controls.js
+++ Connectopia-master/src/Components/Controls.js
@@ -1,30 +1,27 @@
-import { useEffect, useMemo, useRef, useState } from "react"
-import {BsFillMicFill,BsFillCameraVideoFill, BsCameraVideoFill} from 'react-icons/bs'
-import {TbScreenShare} from 'react-icons/tb'
-import {
-  MeetingProvider,
-  MeetingConsumer,
-  useMeeting,
-  useParticipant,
-  Constants,
-} from "@videosdk.live/react-sdk";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { BsFillMicFill, BsFillMicMuteFill, BsFillCameraVideoFill, BsCameraVideoFill } from 'react-icons/bs';
+import { TbScreenShare } from 'react-icons/tb';
+import { MeetingProvider, MeetingConsumer, useMeeting, useParticipant, Constants, } from "@videosdk.live/react-sdk";
 
 function Controls() {
-  const { leave, toggleMic, toggleWebcam, startHls, stopHls,end,enableScreenShare, disableScreenShare, toggleScreenShare } = useMeeting();
+  const { leave, toggleMic, toggleWebcam, startHls, stopHls, end, enableScreenShare, disableScreenShare, toggleScreenShare, isMicMuted } = useMeeting();
+  
+  const micIcon = isMicMuted ? <BsFillMicMuteFill /> : <BsFillMicFill />;
+  
   return (
     <div>
       <button className="btn btn-danger me-2" onClick={() => leave()}>Leave</button>
       <button className="btn btn-danger" onClick={() => end()}>end</button>
       &emsp;|&emsp;
-      
-      <button className="btn btn-light me-2" onClick={() => toggleMic()}><BsFillMicFill/></button>
-      <button className="btn btn-light" onClick={() => toggleWebcam()}><BsFillCameraVideoFill/></button>
+
+      <button className="btn btn-light me-2" onClick={() => toggleMic()}>{micIcon}</button>
+      <button className="btn btn-light" onClick={() => toggleWebcam()}><BsFillCameraVideoFill /></button>
       &emsp;|&emsp;
-      <button className="btn btn-light me-2" onClick={() => toggleScreenShare()}><TbScreenShare/></button>
+      <button className="btn btn-light me-2" onClick={() => toggleScreenShare()}><TbScreenShare /></button>
       <button className="btn btn-light me-2"
         onClick={() => {
-          //We will start the HLS in SPOTLIGHT mode and PIN as
-          //priority so only speakers are visible in the HLS stream
+          // We will start the HLS in SPOTLIGHT mode and PIN as
+          // priority so only speakers are visible in the HLS stream
           startHls({
             layout: {
               type: "SPOTLIGHT",
@@ -45,4 +42,5 @@ function Controls() {
   );
 }
 
-export default Controls
+export default Controls;
+
