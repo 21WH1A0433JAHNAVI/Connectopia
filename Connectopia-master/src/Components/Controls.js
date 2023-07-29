7d7b172e1c8a7599c24da66af9f28d092b0857d8
praneethak <pkanvapuri@gmail.com>
2023-07-29 08:56:26 +0530

    Your commit message

--- Connectopia-master/src/Components/Controls.js
+++ Connectopia-master/src/Components/Controls.js
@@ -1,20 +1,31 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { BsFillMicFill, BsFillMicMuteFill, BsFillCameraVideoFill, BsCameraVideoFill } from 'react-icons/bs';
 import { TbScreenShare } from 'react-icons/tb';
-import { MeetingProvider, MeetingConsumer, useMeeting, useParticipant, Constants, } from "@videosdk.live/react-sdk";
+import { useMeeting } from "@videosdk.live/react-sdk";
 
 function Controls() {
-  const { leave, toggleMic, toggleWebcam, startHls, stopHls, end, enableScreenShare, disableScreenShare, toggleScreenShare, isMicMuted } = useMeeting();
+  const { leave, toggleMic, toggleWebcam, startHls, stopHls, end, isMicMuted } = useMeeting();
   
-  const micIcon = isMicMuted ? <BsFillMicMuteFill /> : <BsFillMicFill />;
-  
+  const [micMuted, setMicMuted] = useState(isMicMuted);
+
+  useEffect(() => {
+    setMicMuted(isMicMuted);
+  }, [isMicMuted]);
+
+  const micIcon = micMuted ? <BsFillMicMuteFill /> : <BsFillMicFill />;
+
+  const handleMicToggle = () => {
+    toggleMic();
+    setMicMuted(!micMuted);
+  };
+
   return (
     <div>
       <button className="btn btn-danger me-2" onClick={() => leave()}>Leave</button>
       <button className="btn btn-danger" onClick={() => end()}>end</button>
       &emsp;|&emsp;
 
-      <button className="btn btn-light me-2" onClick={() => toggleMic()}>{micIcon}</button>
+      <button className="btn btn-light me-2" onClick={handleMicToggle}>{micIcon}</button>
       <button className="btn btn-light" onClick={() => toggleWebcam()}><BsFillCameraVideoFill /></button>
       &emsp;|&emsp;
       <button className="btn btn-light me-2" onClick={() => toggleScreenShare()}><TbScreenShare /></button>
@@ -43,4 +54,5 @@ function Controls() {
 }
 
 export default Controls;
+
 
